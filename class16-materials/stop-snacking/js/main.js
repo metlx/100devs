ab303ed8f2f9c76d16171481382d6e36b708e00e
metlx <metlx>
2022-03-03 23:16:20 -0500

    homework

--- class16-materials/stop-snacking/js/main.js
+++ class16-materials/stop-snacking/js/main.js
@@ -1,1 +1,11 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+
+document.querySelector('#help').addEventListener('click', run)
+
+function run() {
+  let x = document.querySelector('#input').value
+  document.querySelector('#stops').innerText = ""
+  for (let y = 0; y < x; y++) {
+    document.querySelector('#stops').innerText += " STOP "
+  }
+}
--- class16-materials/variable-fun-time/js/main.js
+++ class16-materials/variable-fun-time/js/main.js
@@ -1,18 +1,24 @@
 //--- Easy
 //create a variable and assign it a number
+let x = 3
 
 //minus 10 from that number
-
+x = x - 10
 //print that number to the console
-
+console.log(x)
 //--- Medium
 //create a variable that holds a value from the input
-
+let y = prompt("Enter value: ")
 //add 25 to that number
-
+y = y + 25
 //alert that number
-
+alert(y)
 //--- Hard
 //create a variable that holds the h1
-
+let g = document.querySelector('#id').value
 //add an event listener to that element that console logs the sum of the two previous variables
+document.querySelector('#id').addEventListener('click', run)
+
+function run() {
+  console.log(y + x)
+}
